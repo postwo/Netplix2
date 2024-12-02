7ad9e025f89fc80f3339b8155d19e21b38a24082
postwo <codo7717@naver.com>
2024-12-03 01:46:06 +0900

    netplix-apps / app-api에서     다른 모듈들을 참조할때 패키지이름을 하드코딩한다     하지만 빈껍데기 최상단부분에다가 인터페이스를 만든다. 이렇게 하면 하드코딩을 할 필요가 없다.     참조만 할 수있도록 빈 인터페이스 생성

---
+++ netplix-core/core-usecase/src/main/java/fast.campus.netplix2/UseCaseModule.java
@@ -0,0 +1,8 @@
+package fast.campus.netplix2;
+
+public interface UseCaseModule {
+    /* netplix-apps / app-api에서
+    다른 모듈들을 참조할때 패키지이름을 하드코딩한다
+    하지만 빈껍데기 최상단부분에다가 인터페이스를 만든다. 이렇게 하면 하드코딩을 할 필요가 없다.
+    참조만 할 수있도록 빈 인터페이스 생성*/
+}
