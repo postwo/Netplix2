2d140dc02e943b429abfbc5a85d8c26207281b3f
postwo <codo7717@naver.com>
2024-12-02 19:23:57 +0900

    스프링 애플리케이션 시작

---
+++ netplix-apps/app-api/src/main/java/fast/campus/netplix2/NetplixApiApplication.java
@@ -0,0 +1,11 @@
+package fast.campus.netplix2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetplixApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NetplixApiApplication.class);
+    }
+}
