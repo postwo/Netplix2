29c1adbfcd506393179c549bc5a934da68754cf9
postwo <codo7717@naver.com>
2024-12-05 23:55:40 +0900

    스프링 시큐리티 초기 설정

--- netplix-apps/app-api/build.gradle.kts
+++ netplix-apps/app-api/build.gradle.kts
@@ -4,4 +4,18 @@ dependencies {
     runtimeOnly(project(":netplix-core:core-service"))//runtime 시점에 그모듈을 바라보게 한다.
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.security:spring-security-oauth2-client")
+    implementation("org.springframework.data:spring-data-commons")
+
+}
+
+// api 모듈은 boot jar이름을 알아야 해서 추가
+val appMainClassName = "fast.campus.netplix.NetplixApiApplication"
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    mainClass.set(appMainClassName)
+    archiveClassifier.set("boot")
 }
