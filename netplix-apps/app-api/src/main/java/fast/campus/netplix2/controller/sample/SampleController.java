66c819e9fbecc71bd608522faf28e03753177a4f
postwo <codo7717@naver.com>
2024-12-06 19:47:37 +0900

    import 안된거 다시 등록

--- netplix-apps/app-api/src/main/java/fast/campus/netplix2/controller/sample/SampleController.java
+++ netplix-apps/app-api/src/main/java/fast/campus/netplix2/controller/sample/SampleController.java
@@ -1,5 +1,6 @@
 package fast.campus.netplix2.controller.sample;
 
+import fast.campus.netplix2.sample.SearchSampleUseCase;
 import fast.campus.netplix2.sample.response.SampleResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
